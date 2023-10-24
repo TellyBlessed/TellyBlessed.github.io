d12e4763878e82f31143e5161c0668c206544a2c
TellyBlessed <142826290+TellyBlessed@users.noreply.github.com>
2023-10-24 16:13:32 +0300

    Add files via upload

---
+++ functional programming.py
@@ -0,0 +1,11 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def apply_twice(func, arg):
+...     return func(func(arg))
+... 
+>>> def add_five(x):
+...     return x + 5
+... 
+>>> print(apply_twice(add_five, 10))
+20
+>>> 
---
+++ list comprehension containing if statement.py
@@ -0,0 +1,7 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> evens = [i**2 for i in range(10) if i**2 % 2 == 0]
+>>> 
+>>> print(evens)
+[0, 4, 16, 36, 64]
+>>> 
